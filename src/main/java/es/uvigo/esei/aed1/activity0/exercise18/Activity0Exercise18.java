73fd0a34cc1335ad05947f17a1c2e3739e32a590
icey-e621 <elmagesmm@gmail.com>
2025-02-14 10:56:00 +0100

    added templates

---
+++ src/main/java/es/uvigo/esei/aed1/activity0/exercise18/Activity0Exercise18.java
@@ -0,0 +1,36 @@
+
+package es.uvigo.esei.aed1.activity0.exercise18;
+
+import java.util.Scanner;
+
+
+public class Activity0Exercise18 {
+  
+  //exercise 18
+  /**
+   * muestra un mensaje por pantalla y lee un número entero de teclado
+   * @param message el mensaje a visualizar
+   * @return el número entero leido de teclado
+   */
+  public static int readNumber(String message) {
+    return 0;
+  }
+
+ 
+  /**
+   * rellena y lee una mtriz con numeros aleatorios menores que 10
+   * @param matrix que almacena números reales
+   */
+  public static void readMatrix(int[][] matrix) {
+    
+  }
+    /**
+   * busca un numero {@link num} en la matriz {@link matrix}
+   * @param matrix que almacena números reales
+   * @param num numero que buscar en la matriz
+   */
+  public static void searchMatrix(int[][] matrix, int num) {
+    
+  }
+
+}
